e43d35b8836602af375021d10fbeb0905fbda499
AWCShakil443 <awc.shakil407@gmail.com>
2026-08-27 20:20:05 +0600

    Add official source registry for syllabus and legal/regulatory materials

---
+++ data/source-registry.js
@@ -0,0 +1,11 @@
+window.SOURCE_REGISTRY={verified:'2026-08-27',sources:[
+{name:'ICAB Professional Level Syllabus 2023',url:'https://www.icab.org.bd/icabadmin/uploads/ckeditor/62542.%20ICAB%20Syllabus-2023_%28PDF%20File%29.pdf',kind:'syllabus'},
+{name:'Companies Act 1994',url:'https://bdlaws.minlaw.gov.bd/act-788.html',kind:'act'},
+{name:'BSEC Securities Laws',url:'https://sec.gov.bd/home/laws',kind:'regulator'},
+{name:'Bank Company Act 1991',url:'https://bdlaws.minlaw.gov.bd/act-print-751.html',kind:'act'},
+{name:'Bangladesh Bank Acts and Laws',url:'https://www.bb.org.bd/en/index.php/about/lawsnacts',kind:'regulator'},
+{name:'Finance Company Act 2023 / Bangladesh Bank',url:'https://www.bb.org.bd/en/index.php/about/lawsnacts',kind:'act'},
+{name:'Insurance Act 2010',url:'https://bdlaws.minlaw.gov.bd/act-print-1037.html',kind:'act'},
+{name:'Bangladesh Labour Act 2006',url:'https://bdlaws.minlaw.gov.bd/act-print-952.html',kind:'act'},
+{name:'Foreign Exchange Guidelines 2018 Vol 1',url:'https://www.bb.org.bd/aboutus/regulationguideline/foreignexchange/fullguide_v1.pdf',kind:'guideline'},
+{name:'Foreign Exchange Guidelines 2018 Vol 2',url:'https://www.bb.org.bd/feidportal/guidelines/gudforextransvol2.pdf',kind:'guideline'}]};
